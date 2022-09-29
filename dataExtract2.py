5badc8377abf402f14fa6cac881c253dbc7610f0
rudy-prietno <prietnorudy@gmail.com>
2022-09-29 11:43:00 +0700

    Create dataExtract2.py

---
+++ dataExtract2.py
@@ -0,0 +1,33 @@
+import asyncpg
+import asyncio
+import os
+import sys
+sys.path.append(os.getcwd())
+
+
+class dataExtract_com ():
+    ####### running extract data #######
+    async def extract(connection, query):
+        try:
+            record = await connection.fetch(query)
+
+        except asyncpg.PostgresError as exc:
+                return ("Failed to extract data. ", exc)
+        else:
+            return connection
+            await connection.close()
+
+
+
+    ####### running extract data #######
+    async def extractPool(connection, query):
+        try:
+            async with connection.acquire() as con:
+                record = await con.fetch(query= query)
+                
+        except asyncpg.PostgresError as exc:
+                return ("Failed to extract data. ", exc)
+
+        else:
+            return record
+            await connection.close()
